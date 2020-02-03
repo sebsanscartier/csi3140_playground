088bf913f434f2b21560f6cdf01d90e4f424df8f
Sébastien Sanscartier <ssans075@uottawa.ca>
2020-02-03 14:26:59 -0500

    add the yahtzee game code

---
+++ lab04/09_events.js
@@ -0,0 +1,22 @@
+
+window.onload = function()
+{
+  var roller = document.getElementById("roller");
+  roller.addEventListener(
+    "click", 
+    function () {
+      Dice.showDie("die01", Dice.roll());
+      Dice.showDie("die02", Dice.roll());
+      Dice.showDie("die03", Dice.roll());
+      Dice.showDie("die04", Dice.roll());
+      Dice.showDie("die05", Dice.roll());
+    }, 
+    false);
+
+  Dice.showDie("die01", 0);
+  Dice.showDie("die02", 0);  
+  Dice.showDie("die03", 0);  
+  Dice.showDie("die04", 0);  
+  Dice.showDie("die05", 0);  
+
+}
